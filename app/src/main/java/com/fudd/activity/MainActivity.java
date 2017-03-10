22e95952ef02019c542090cbb174ad385b5fefd3
fudd <5036175@qq.com>
2017-03-10 09:44:58 +0800

    Initial commit

---
+++ app/src/main/java/com/fudd/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.fudd.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 此工程用于记录 《Android UI 基础教程》 学习代码
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
